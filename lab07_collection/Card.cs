a81fd205942d57989c8adb3bbee109ed2588f709
shalina2 <shalom.belaineh@gmail.com>
2019-01-16 19:05:43 -0800

    passed one test

---
+++ lab07_collection/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab07_collection
+{
+    public class Card
+    {
+        public enum Suits { Diamonds,Hearts,Spades,Clubs }
+        public string CardNum { get; set; }
+        public Suits suit { get; set; }
+
+        public Card(String cardNumber,Suits suitName)
+        {
+            CardNum = cardNumber;
+            suit = suitName;
+        }
+    }
+}
---
+++ lab07_collection/Deck.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab07_collection
+{
+    public class Deck<T>: IEnumerable<T> where T : Card
+    {
+        T[] cards = new T[56];
+        int currentIndex = 0;
+        int counter = 0;
+
+        public void Add(T item)
+        {
+            if (currentIndex > cards.Length - 1)
+            {
+                Array.Resize(ref cards, cards.Length * 3);
+            }
+            cards[currentIndex] = item;
+            currentIndex++;
+            counter++;
+        }
+
+      public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < currentIndex; i++)
+            {
+                yield return cards[i];
+            }
+        }
+        public T[] Remove(T item)
+        {
+            for (int i = 0; i < cards.Length; i++)
+            {
+                if (cards[i] == item)
+                {
+                    int index = Array.IndexOf(cards, item);
+                    counter--;
+
+                    T[] newCards = cards.Where((Val, idx) => idx != index).ToArray();
+                    Array.Copy(newCards, cards, newCards.Length);
+                    return cards;
+                }
+            }
+            Console.WriteLine("Not there");
+            return null;
+        }
+
+            public int Count()
+            {
+                return counter;
+            }
+           
+            
+       
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
